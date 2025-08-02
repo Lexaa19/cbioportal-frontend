17126d668176940165c2336413ef94b98916318a
Lexaa19 <caliopi.durdunescu@yahoo.com>
2025-08-02 11:49:40 +0300

    Add  Railway deployment

--- packages/oncoprintjs/server.js
+++ packages/oncoprintjs/server.js
@@ -5,7 +5,7 @@ server.use(express.static(__dirname + '/dist'));
 server.use(express.static(__dirname + '/rules'));
 server.use(express.static(__dirname + '/test'));
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 server.listen(port, function() {
     console.log('View Oncoprint at http://localhost:' + port + '/index.html');
 });
